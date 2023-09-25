fb763d0b2795cdf3e6de59347820f77ac34f38ba
5hubham5ingh <shubham747474singh@gmail.com>
2023-09-25 21:58:56 +0530

    DB connected

--- Server/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = "mongodb+srv://Admin:77529875379795801165@book-store-mern.majqbhd.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasApp";
