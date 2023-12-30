6e7e20a77469dd22dda557741bfa19778b00a91b
Shubham Singh <shubham.singh@mygstcafe.in>
2023-12-30 16:07:24 +0530

    home page

--- Client/src/Modules/Pages/Home.jsx
+++ Client/src/Modules/Pages/Home.jsx
@@ -1,12 +1,308 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Home() {
-  const navigate = useNavigate();
-  useEffect(()=>{navigate('/logIn')})
+export default function Component() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col min-h-screen bg-blue-50">
+      <header className="px-4 lg:px-6 h-14 flex items-center bg-blue-600 text-white">
+        <a className="flex items-center justify-center" href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="h-6 w-6"
+            style="--darkreader-inline-stroke: currentColor;"
+            data-darkreader-inline-stroke=""
+          >
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+          </svg>
+          <span className="ml-2">TaskMaster</span>
+        </a>
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Home
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Features
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Pricing
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            About
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Contact
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Login
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Logout
+          </a>
+          <a
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="#"
+          >
+            Workspace
+          </a>
+        </nav>
+      </header>
+      <main className="flex-1">
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-white">
+          <div className="container px-4 md:px-6">
+            <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+              <img
+                alt="Hero"
+                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
+                height="550"
+                src="/placeholder.svg"
+                width="550"
+              />
+              <div className="flex flex-col justify-center space-y-4">
+                <div className="space-y-2">
+                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                    Accomplish More Together
+                  </h1>
+                  <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                    TaskMaster is the ultimate collaboration platform for modern
+                    teams. Stay organized, track progress, and deliver results.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-white">
+          <div className="container px-4 md:px-6">
+            <div className="grid gap-6 md:grid-cols-3">
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="w-8 h-8"
+                    style="--darkreader-inline-stroke: currentColor;"
+                    data-darkreader-inline-stroke=""
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                  <h3 className="text-xl font-bold">Task Assignment</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    Assign tasks to team members, set deadlines, and track
+                    progress all in one place.
+                  </p>
+                </div>
+              </div>
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="w-8 h-8"
+                    style="--darkreader-inline-stroke: currentColor;"
+                    data-darkreader-inline-stroke=""
+                  >
+                    <line x1="12" x2="12" y1="20" y2="10"></line>
+                    <line x1="18" x2="18" y1="20" y2="4"></line>
+                    <line x1="6" x2="6" y1="20" y2="16"></line>
+                  </svg>
+                  <h3 className="text-xl font-bold">Progress Tracking</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    Visualize your team's progress with our intuitive dashboard.
+                    Stay on top of your tasks and deadlines.
+                  </p>
+                </div>
+              </div>
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="w-8 h-8"
+                    style="--darkreader-inline-stroke: currentColor;"
+                    data-darkreader-inline-stroke=""
+                  >
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                  <h3 className="text-xl font-bold">Real Time Collaboration</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    Collaborate with your team in real-time. Share ideas,
+                    documents, and feedback instantly.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-white">
+          <div className="container px-4 md:px-6">
+            <h2 className="text-3xl font-bold mb-6">What Our Users Say</h2>
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <span
+                    className="relative flex shrink-0 overflow-hidden rounded-full w-12 h-12"
+                    src="/placeholder.svg?height=50&amp;width=50"
+                  ></span>
+                  <h3 className="text-xl font-bold">John Doe</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    "TaskMaster has completely transformed the way our team
+                    works. It's a game changer."
+                  </p>
+                </div>
+              </div>
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <span
+                    className="relative flex shrink-0 overflow-hidden rounded-full w-12 h-12"
+                    src="/placeholder.svg?height=50&amp;width=50"
+                  ></span>
+                  <h3 className="text-xl font-bold">Jane Smith</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    "I love how easy it is to assign tasks and track progress.
+                    TaskMaster has made project management a breeze."
+                  </p>
+                </div>
+              </div>
+              <div
+                className="rounded-lg border bg-card text-card-foreground shadow-sm"
+                data-v0-t="card"
+              >
+                <div className="flex flex-col space-y-1.5 p-6">
+                  <span
+                    className="relative flex shrink-0 overflow-hidden rounded-full w-12 h-12"
+                    src="/placeholder.svg?height=50&amp;width=50"
+                  ></span>
+                  <h3 className="text-xl font-bold">Alex Johnson</h3>
+                </div>
+                <div className="p-6">
+                  <p>
+                    "The real-time collaboration feature is amazing. Our team
+                    can now work together seamlessly."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-blue-600 text-white">
+        <p className="text-xs">© TaskMaster. All rights reserved.</p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <a className="text-xs hover:underline underline-offset-4" href="#">
+            Privacy Policy
+          </a>
+          <a className="text-xs hover:underline underline-offset-4" href="#">
+            Terms of Service
+          </a>
+          <a className="text-xs hover:underline underline-offset-4" href="#">
+            Contact
+          </a>
+        </nav>
+        <div className="flex gap-4 mt-2 sm:mt-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="w-4 h-4"
+            style="--darkreader-inline-stroke: currentColor;"
+            data-darkreader-inline-stroke=""
+          >
+            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+          </svg>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default Home
+// function Home() {
+//   const navigate = useNavigate();
+//   useEffect(()=>{navigate('/logIn')})
+//   return (
+//     <div>Home</div>
+//   )
+// }
+
+// export default Home
