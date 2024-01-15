6df392edcbd88c770f149b8265e14c5a49fbebd1
Shubham Singh <shubham.singh@mygstcafe.in>
2024-01-15 18:43:17 +0530

    added guest buttons in navbar of homepage

--- Client/src/Modules/Components/Home/Navbar.jsx
+++ Client/src/Modules/Components/Home/Navbar.jsx
@@ -1,9 +1,35 @@
 import { Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import logo from "../../Assets/logo.svg";
+import { userActions } from "../../Features/User/userSlice";
+import { useDispatch } from "react-redux";
+import { useUser } from "../../Features/User/userSelectors";
+
 export default function Navbar() {
   const navigate = useNavigate();
-
+  const user =  useUser();
+  const dispatch = useDispatch();
+  const userButtons = [
+    {
+      name: "Workspaces",
+      action: () => navigate("/workspaces"),
+    },
+    {
+      name: "Log out",
+      action: () => dispatch(userActions.logOut()),
+    },
+  ];
+  const guestButtons = [
+    {
+      name: "Log in",
+      action: () => navigate("/logIn"),
+    },
+    {
+      name: "Sign up",
+      action: () => navigate("/signUp"),
+    },
+  ];
+console.log('user',user)
   return (
     <nav style={navBarStyle}>
       <div style={logoAndNameContaier}>
@@ -13,48 +39,51 @@ export default function Navbar() {
         </Typography>
       </div>
       <div>
-        <Button style={buttonStyle} onClick={() => navigate("/logIn")}>
-          Log In
-        </Button>
-        <Button style={buttonStyle} onClick={() => navigate("/signUp")}>
-          Sign Up
-        </Button>
+        {user
+          ? userButtons.map((button) => (
+              <Button style={buttonStyle} key={button.name} onClick={button.action}>
+                {button.name}
+              </Button>
+            ))
+          : guestButtons.map((button) => 
+              <Button style={buttonStyle} key={button.name} onClick={button.action}>
+                {button.name}
+              </Button>
+            )}
       </div>
     </nav>
   );
 }
 
+const navBarStyle = {
+  backgroundColor: "rgb(55, 55, 114)",
+  color: "white",
+  border: "none",
+  borderBottomRightRadius: "30px",
+  paddingLeft: "2%",
+  paddingRight: "2%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  position: "fixed",
+  top: "0",
+  left: "0",
+  right: "0",
+  width: "90%",
+};
 
-  
-  const navBarStyle = {
-    backgroundColor: "rgb(55, 55, 114)",
-    color: "white",
-    border: "none",
-    borderBottomRightRadius: "30px",
-    paddingLeft: "2%",
-    paddingRight: "2%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    position: "fixed",
-    top: "0",
-    left: "0",
-    right: "0",
-    width: "90%",
-  };
-  
-  const buttonStyle = {
-    color: "#abdaed",
-  };
+const buttonStyle = {
+  color: "#abdaed",
+};
 
-  const logoAndNameContaier = {
-    display: "flex",
-    alignItems: "center",
-    cursor: "pointer",
-  };
-  
-  const logoStyle = {
-    height: "50px",
-    width: "50px",
-    marginRight: "5px",
-  };
+const logoAndNameContaier = {
+  display: "flex",
+  alignItems: "center",
+  cursor: "pointer",
+};
+
+const logoStyle = {
+  height: "50px",
+  width: "50px",
+  marginRight: "5px",
+};
