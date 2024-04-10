d1d3ef666cfd8feefa3c8e5597ab7af6b286e544
Shubham Singh <128460722+5hubham5ingh@users.noreply.github.com>
2024-04-10 17:51:51 +0530

    config setup

--- Server/configs/config.js
+++ Server/configs/config.js
@@ -1,12 +1,15 @@
+import { config } from "dotenv";
+
+config();
 export default {
-    PORT: process.env.PORT || 3000,
-    MONGO_URI : process.env.MONGO_URI || null,
-    JWT_KEY: process.env.JWT_KEY || 'workspce',
-    JWT_REFRESH_KEY: process.env.JWT_REFRESH_KEY || 'workspace',
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    SESSION_DURATION: process.env.SESSION_DURATION || "1h",
-    EXTENDED_SESSION_DURATION: process.env.EXTENDED_SESSION_DURATION || "1d",
-    
+  PORT: process.env.PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI || null,
+  JWT_KEY: process.env.JWT_KEY || 'workspce',
+  JWT_REFRESH_KEY: process.env.JWT_REFRESH_KEY || 'workspace',
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  SESSION_DURATION: process.env.SESSION_DURATION || "1h",
+  EXTENDED_SESSION_DURATION: process.env.EXTENDED_SESSION_DURATION || "1d",
+
 
 
 }
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import { config } from "dotenv";
 import cors from "cors";
 import { authRoutes } from "./routes/auth.js";
 import { workspaceRoutes } from "./routes/workspace.js";
@@ -11,12 +10,11 @@ import cookieParser from "cookie-parser";
 import { errorConverter, errorHandler } from "./middleware/error.js";
 import authLimiter from "./middleware/rateLimiter.js";
 import helmet from "helmet";
-import {xss} from "express-xss-sanitizer";
+import { xss } from "express-xss-sanitizer";
 import mongoSanitize from "express-mongo-sanitize";
 import compression from "compression";
 import server_config from "./configs/config.js"
 
-config();
 const app = express();
 
 // set security HTTP headers
@@ -40,23 +38,23 @@ app.use(compression());
 
 
 // limit repeated failed requests to auth endpoints
-app.use('/auth',authLimiter)
+app.use('/auth', authLimiter)
 
-app.use('/auth',authRoutes);
+app.use('/auth', authRoutes);
 
 app.use(validateUserSession)
 
-app.use('/user',workspacesRoutes);
+app.use('/user', workspacesRoutes);
 
-app.use('/workspace',workspaceRoutes);
+app.use('/workspace', workspaceRoutes);
 
 
 app.get("/", (request, response) => {
-    console.log(request);
-    return response.status(234).send("Welcome to Workspace server.");
-  });
+  console.log(request);
+  return response.status(234).send("Welcome to Workspace server.");
+});
 
-  // convert error to ApiError, if needed
+// convert error to ApiError, if needed
 app.use(errorConverter);
 
 // handle error
@@ -65,14 +63,14 @@ app.use(errorHandler);
 
 // connect to DB
 
-const mongoDbUri = process.env.MONGO_URI;
-const PORT = process.env.PORT;
+const mongoDbUri = server_config.MONGO_URI;
+const PORT = server_config.PORT;
 mongoose
   .connect(mongoDbUri)
   .then(() => {
     console.log("Connected to DB");
     app.listen(PORT, () => {
-        console.log(`App is listening to ${PORT}`);
-      });
+      console.log(`App is listening to ${PORT}`);
+    });
   })
-  .catch((error) => {console.log("Error in DB connection: ",error)});
+  .catch((error) => { console.log("Error in DB connection: ", error) });
